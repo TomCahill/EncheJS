b7fa6c5c483a102541ef6b09eddaa4f9408d2af7
Tom Cahill <me@tomcahill.co.uk>
2017-06-16 19:42:09 +0100

    - ADDED: Shitty preloading

--- app_client/src/js/game.js
+++ app_client/src/js/game.js
@@ -10,6 +10,8 @@ class Game { // eslint-disable-line no-unused-vars
   constructor(gameMainFrameId) {
     console.log('Game:constructor');
 
+    this._gameMainFrameId = gameMainFrameId;
+
     this.socket = null;
     this.input = null;
     this.network = null;
@@ -28,23 +30,34 @@ class Game { // eslint-disable-line no-unused-vars
     this._frameRate = 0;
     this._fps = 0;
 
-    this._init(gameMainFrameId);
+    this._assets = [
+      { path: '/assets/images/sprites/maps/test.png', type: 'image' },
+      { path: '/assets/images/sprites/maps/test.png', type: 'image' },
+      { path: '/assets/images/sprites/player.png', type: 'image' }
+    ];
+    this._preloadLoaded = false;
+    this._preloadProgress = 0;
+
+    this._preloadAssets();
+    this._init();
   }
 
   /**
    *
-   * @param {string} gameMainFrameId  - Main game canvas element id
    * @private
    */
-  _init(gameMainFrameId) {
+  _init() {
     console.log('Game:_init');
     this.input = new Input();
-    this.canvas = new Canvas(gameMainFrameId);
+    this.canvas = new Canvas(this._gameMainFrameId);
 
     this.viewPort = new ViewPort(this.canvas.size);
 
-    this.map = new Map(this.input);
+    this._main();
+  }
 
+  _initGame() {
+    this.map = new Map(this.input);
     this.player = new Player(this.input, this.map);
 
     let socketIO = null;
@@ -53,8 +66,23 @@ class Game { // eslint-disable-line no-unused-vars
     }
 
     this.network = new Network(socketIO);
+  }
 
-    this._main();
+  _preloadAssets() {
+    console.log('Game:_preload');
+    for(let i = 0; i < this._assets.length; i++) {
+      let asset = this._assets[i];
+      if (asset.type === 'image') {
+        asset.element = new Image();
+        asset.element.src = asset.path;
+        asset.element.onload = () => {
+          asset.loaded = true;
+          this._preloadProgress++;
+        };
+      }
+
+    }
+    // this._init();
   }
 
   /**
@@ -93,6 +121,13 @@ class Game { // eslint-disable-line no-unused-vars
    */
   _update(delta) {
     // console.log('Game:_update');
+    if (!this._preloadLoaded) {
+      if (this._preloadProgress / this._assets.length === 1) {
+        this._initGame();
+        this._preloadLoaded = true;
+      }
+      return;
+    }
     this.player.update(delta);
     this.viewPort.update(delta, this.player.position);
     this.map.update(delta);
@@ -107,11 +142,34 @@ class Game { // eslint-disable-line no-unused-vars
     // console.log('Game:_render');
     context.clearRect(0, 0, this.canvas.size.x, this.canvas.size.y);
 
+    if (!this._preloadLoaded) {
+      this._renderLoader(context);
+      return;
+    }
+
     this.map.render(context, this.viewPort.offset);
     this.player.render(context, this.viewPort.offset);
 
     this.map.postRender(context, this.viewPort.offset);
 
+    this._renderDebug(context);
+  }
+
+  _renderLoader(context) {
+    context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+    context.fillRect(
+      0,
+      0,
+      this.canvas.size.x * (this._preloadProgress / this._assets.length),
+      this.canvas.size.y
+    );
+
+    context.font = '20px Arial';
+    context.fillStyle = '#FFF';
+    context.fillText(`Loading: ${this._preloadProgress}/${this._assets.length}`, 20, 40);
+  }
+
+  _renderDebug(context) {
     // Debug shit
     context.font = '20px Arial';
     context.fillStyle = '#FFF';
